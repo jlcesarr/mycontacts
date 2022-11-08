09ea7911668ebce78c0f99938b710f093cab8310
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-08 16:30:38 -0300

    feat: create contacts use case

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,6 +1,11 @@
-class ContactController {
-  index() {
+/* eslint-disable import/extensions */
+import ContactsRepository from '../../repositories/ContactsRepository.js';
 
+class ContactController {
+  async index(request, response) {
+    const contactsRepository = ContactsRepository;
+    const allContacts = await contactsRepository.findAll();
+    return response.json(allContacts);
   }
 }
 

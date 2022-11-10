dac2ed47415c03fba29cbcba53831a8d54db8aa8
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-10 13:25:45 -0300

    feat: add support to handle async errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 /* eslint-disable import/extensions */
 import express from 'express';
+import 'express-async-errors';
 import { router as routes } from './routes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((err, request, response, _) => response.status(500).json({ error: 'Internal Server Error' }));
 
 app.listen(3000, () => {
   console.log('Server is listening!');

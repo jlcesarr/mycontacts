6ae5b959d852d69967e80b4c76473d894d67ff0c
Julio Cesar <jcesarpereira2021@gmail.com>
2022-11-10 16:08:58 -0300

    feat(database): change database connection credentials to env vars
    This provide an automate way to create a postgres server using docker
    The schema.sql file autosetup after the 'docker compose up' command is executed
    If the database connection fails, the application will stop
    The PG node client and the compose file uses the same credentials set in .env file

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -1,16 +1,27 @@
 import pg from 'pg';
+import dotenv from "dotenv"
+
+if(process.env.NODE_ENV != "production"){
+  dotenv.config();
+}
 
 const client = new pg.Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'root',
-  password: 'root',
-  database: 'mycontacts',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
-client.connect();
+client.connect()
+.then(() => console.log("Database is ready!"))
+.catch(() => {
+  console.log("Database connection failed! Exiting process...")
+  process.exit()
+});
 
 export async function execQuery(query, values) {
+
   const { rows } = await client.query(query, values);
   return rows;
 }
